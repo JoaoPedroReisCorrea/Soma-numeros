34b24a83d1bd3996627192a285935045fa6270f1
João Pedro Reis <j_pedro45@yahoo.com>
2025-08-08 09:10:52 -0300

    Terceiro commit

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-let botao = document.getElementById("somar");
-let input1 = document.getElementById("numero1");
-let input2 = document.getElementById("numero2");
-let resultado = document.getElementById("resultado");
+const btnSomar = document.getElementById("btnSomar");
 
-botao.onclick = function() {
-  let num1 = Number(input1.value);
-  let num2 = Number(input2.value);
-
+btnSomar.onclick = function() {
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
   let soma = num1 + num2;
 
-  resultado.textContent = "Resultado: " + soma;
+  document.getElementById("resultado").textContent = "Resultado: " + soma;
 };
